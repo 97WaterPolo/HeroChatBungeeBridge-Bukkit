35d6909c06f320ac3e50b0f575b89b93d5d43455
97WaterPolo <alexsigler@sws-llc.com>
2014-02-28 19:39:32 -0800

    Added an option based off the config in which to set the prefix as well as the prefix message.

--- src/com/jumanjicraft/BungeeChatClient/BungeeChatListener.java
+++ src/com/jumanjicraft/BungeeChatClient/BungeeChatListener.java
@@ -14,44 +14,57 @@ import com.google.common.io.ByteStreams;
 
 public class BungeeChatListener implements PluginMessageListener {
 
-        static BungeeChatClient plugin;
- 
-        public BungeeChatListener(BungeeChatClient plugin) {
-                BungeeChatListener.plugin = plugin;
-                Bukkit.getMessenger().registerIncomingPluginChannel(plugin, "BungeeChat", this);
-                Bukkit.getMessenger().registerOutgoingPluginChannel(plugin, "BungeeChat");
-        }
- 
-        public static void TransmitChatMessage(String message, String chatchannel) {
-               ByteArrayDataOutput out = ByteStreams.newDataOutput();
-               out.writeUTF(chatchannel);
-               out.writeUTF(message);
-               Bukkit.getOnlinePlayers()[0].sendPluginMessage(plugin, "BungeeChat", out.toByteArray());
-        }
- 
-        public void onPluginMessageReceived(String s, Player player, byte[] bytes) {
-			if (!s.equalsIgnoreCase("BungeeChat"))
+	static BungeeChatClient plugin;
+
+	public BungeeChatListener(BungeeChatClient plugin) {
+		BungeeChatListener.plugin = plugin;
+		Bukkit.getMessenger().registerIncomingPluginChannel(plugin, "BungeeChat", this);
+		Bukkit.getMessenger().registerOutgoingPluginChannel(plugin, "BungeeChat");
+	}
+
+	public static void TransmitChatMessage(String message, String chatchannel) {
+		ByteArrayDataOutput out = ByteStreams.newDataOutput();
+		out.writeUTF(chatchannel);
+		out.writeUTF(message);
+		Bukkit.getOnlinePlayers()[0].sendPluginMessage(plugin, "BungeeChat", out.toByteArray());
+	}
+
+	public void onPluginMessageReceived(String s, Player player, byte[] bytes) {
+		if (!s.equalsIgnoreCase("BungeeChat"))
+		{
+			return;
+		}
+		ByteArrayDataInput in = ByteStreams.newDataInput(bytes);
+		String chatchannel = in.readUTF();
+		String message = in.readUTF();
+		message = ChatColor.translateAlternateColorCodes('&', message);
+		Channel channel = Herochat.getChannelManager().getChannel(chatchannel);
+		if (channel == null)
+		{
+			Bukkit.getLogger().warning("Channel "+chatchannel+" doesn't exist, but a message was receieved on it. Your Herochat configs aren't probably the same on each server.");
+			return;
+		} 
+		String prefix = plugin.getConfig().getString("prefix.newPrefix");
+		StringBuilder msg = new StringBuilder(channel.applyFormat(channel.getFormatSupplier().getAnnounceFormat(), "").replace("%2$s", message.replaceAll("(?i)&([a-fklmno0-9])", "\247$1")));
+		if (plugin.getConfig().getBoolean("prefix") == true)
+		{
+			if (channel.getFormat().startsWith("["))
 			{
-				return;
+				msg.deleteCharAt(2);
+				channel.sendRawMessage(ChatColor.GREEN + prefix + ChatColor.RESET+msg);
+			} else {
+				channel.sendRawMessage(ChatColor.GREEN + prefix + ChatColor.RESET+msg);
 			}
-        	ByteArrayDataInput in = ByteStreams.newDataInput(bytes);
-        	String chatchannel = in.readUTF();
-        	String message = in.readUTF();
-        	message = ChatColor.translateAlternateColorCodes('&', message);
-        	Channel channel = Herochat.getChannelManager().getChannel(chatchannel);
-        	if (channel == null)
-        	{
-        		Bukkit.getLogger().warning("Channel "+chatchannel+" doesn't exist, but a message was receieved on it. Your Herochat configs aren't probably the same on each server.");
-				return;
-        	} 
-        	StringBuilder msg = new StringBuilder(channel.applyFormat(channel.getFormatSupplier().getAnnounceFormat(), "").replace("%2$s", message.replaceAll("(?i)&([a-fklmno0-9])", "\247$1")));
+		}else
+		{
         	if (channel.getFormat().startsWith("["))
         	{
         		msg.deleteCharAt(2);
-        		channel.sendRawMessage(ChatColor.GREEN+"[N"+ChatColor.RESET+msg);
+        		channel.sendRawMessage(ChatColor.GREEN + "" + ChatColor.RESET + msg);
         	} else {
-        		channel.sendRawMessage(ChatColor.GREEN+"[N]"+ChatColor.RESET+msg);
+        		channel.sendRawMessage(ChatColor.GREEN + "" + ChatColor.RESET + msg);
         	}
-        }
-        
+		}
+	}
+
 }
